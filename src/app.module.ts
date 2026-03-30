8c3eb35aa236f9e8fce1012faac72c64434772e6
najafsikander <najafsikander@yahoo.com>
2026-03-30 17:06:00 +0400

    Added rate limitter and basic logging - need to add custom logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    //Applying RateLimitter
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
     bodyParser: true,
+    logger: new ConsoleLogger({
+      prefix: 'NestJs-Practice',
+      timestamp: true,
+      logLevels: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
+      compact: true,
+    }),
   });
 
   app.use(helmet());
